0455c5783df08e859f9ed2af9c5e3459aa351e84
yonezawa-sora <m224825@hiroshima-u.ac.jp>
2024-01-13 02:49:22 +0000

    Add call_esearch_pmc function

--- PubmedZenbu/eutils.py
+++ PubmedZenbu/eutils.py
@@ -1,10 +1,6 @@
-import re
-import datetime
 import requests
 import xml.etree.ElementTree as ET
-import subprocess
 from urllib.request import Request, urlopen
-
 
 def get_yearlist(minyear:int):
     """
@@ -15,15 +11,17 @@ def get_yearlist(minyear:int):
     Returns:
     --------
     years_list: list
-        A list of year starting from minyear to 2023.
+        A list of year starting from minyear to 2024.
     """
-    years_list = [str(x) for x in range(minyear, 2024)]
+    years_list = [str(x) for x in range(minyear, 2025)]
     return years_list
 
 
 def call_esearch(query_str: str, mindate:int) -> ET.Element:
     """
-    10000件までしか取れないので、mindateを指定して、1年ずつPMIDを取得する
+    description:
+    ------
+    Since a maximum of 10,000 records can be retrieved at a time, the 'mindate' is specified to obtain PMIDs year by year.
 
     Parameters:
     ------
@@ -37,6 +35,28 @@ def call_esearch(query_str: str, mindate:int) -> ET.Element:
     url = f"https://eutils.ncbi.nlm.nih.gov/entrez/eutils/esearch.fcgi?db=pubmed&term={query_str}&retmax=10000&mindate={mindate}&maxdate={mindate}"
     tree = use_eutils(url)
     return tree
+
+
+def call_esearch_pmc(query_str: str, mindate:int) -> ET.Element:
+    """
+    description:   
+    ------
+    Since a maximum of 10,000 records can be retrieved at a time, the 'mindate' is specified to obtain PMCIDs year by year.
+
+    Parameters:
+    ------
+        query_str: str
+        retmax: int
+        year: int
+
+    Returns:
+    ------
+        tree: xml
+    """
+    pmc_url = f"https://www.ncbi.nlm.nih.gov/pmc/utils/esearch.fcgi?db=pmc&term={query_str}&retmax=10000&mindate={mindate}&maxdate={mindate}"
+    tree = use_eutils(pmc_url)
+    return tree
+
 
 
 def generate_chunked_id_list(id_list, max_len) -> list:
