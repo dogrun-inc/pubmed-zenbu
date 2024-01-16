722e4ed1719bc75aea71af9a453bb87404095eb8
米澤奏良（Yonezawa Sora） <m224825@hiroshima-u.ac.jp>
2024-01-16 03:03:27 +0000

    add gpt4_api function

--- PubmedZenbu/use_gpt.py
+++ PubmedZenbu/use_gpt.py
@@ -18,3 +18,18 @@ def gpt_api(prompt,openai_api_key):
     # return response
     return response["choices"][0]["text"].replace("\n", "")
 
+def gpt4_api(prompt, openai_api_key):
+    openai.api_key = openai_api_key
+    response = openai.ChatCompletion.create(
+        model="gpt-4-1106-preview",  
+        messages=[
+            {"role": "user", "content": prompt}
+        ],
+        max_tokens=1000,  
+        temperature=0.01,  
+        frequency_penalty=0.0, 
+        presence_penalty=0.0  
+    )
+    # return response["choices"][0]["text"].replace("\n',")
+    # return response
+    return response["choices"][0]["message"]["content"].replace("\n", "")
